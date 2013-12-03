8bcb21cb1e3682ac48af8309b9bc135ef92b2893
Andre Ambrosio Boechat <boechat107@gmail.com>
2013-12-03 21:35:14 -0200

    Dependencies added; necessary classes to repeat BoofCV's example
    http://boofcv.org/index.php?title=Example_Fundamental_Matrix

--- banknote-processing/project.clj
+++ banknote-processing/project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojars.boechat107/eye-boof "0.1.0-SNAPSHOT"]
+                 ])
--- banknote-processing/src/banknote_processing/core.clj
+++ banknote-processing/src/banknote_processing/core.clj
@@ -1,4 +1,12 @@
-(ns banknote-processing.core)
+(ns banknote-processing.core
+  (:require 
+    [eye-boof
+     [core :as c]])
+  (:import 
+    [boofcv.factory.feature.detdesc FactoryDetectDescribe]
+    [boofcv.abst.feature.detdesc DetectDescribePoint]
+    [boofcv.factory.feature.associate FactoryAssociation]
+    [boofcv.abst.feature.associate ScoreAssociation]))
 
 (defn foo
   "I don't do a whole lot."
