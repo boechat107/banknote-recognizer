f065c5428192ebe4143499a3937bdc0f005f90dd
Andre Ambrosio Boechat <boechat107@gmail.com>
2013-12-09 16:04:02 -0200

    Detection and association of interested points
    * A better function sign is necessary, like returning a function that
    receives just two images as arguments. The argument of the parent
    function should be the detection algorithm and the scorer.

--- banknote-processing/src/banknote_processing/core.clj
+++ banknote-processing/src/banknote_processing/core.clj
@@ -1,7 +1,11 @@
 (ns banknote-processing.core
   (:require 
     [eye-boof
-     [core :as c]])
+     [core :as c]
+     [helpers :as h]
+     [visualize :as v]
+     [processing :as p]
+     ])
   (:import 
     [boofcv.factory.feature.detdesc FactoryDetectDescribe]
     [boofcv.abst.feature.detdesc DetectDescribePoint]
@@ -9,19 +13,59 @@
     [boofcv.abst.feature.associate ScoreAssociation]
     [boofcv.abst.feature.detect.interest ConfigFastHessian]
     [boofcv.struct.feature SurfFeature]
-    [boofcv.struct.image ImageFloat32]
+    [boofcv.struct.image ImageFloat32 ImageSInt32]
+    [boofcv.alg.feature UtilFeature]
+    [java.util ArrayList]
+    
+    [boofcv.gui.feature AssociationPanel]
+    [boofcv.gui.image ShowImages]
     ))
 
-(defn get-associations)
+(defn get-associations
+  [img1 img2 ^DetectDescribePoint feat-alg scorer]
+  (let [;; Function to detect the interested points of an image and returns their
+        ;; locations and descriptions.
+        get-interested-pt (fn [img]
+                            (let [pos-array (ArrayList.)
+                                  desc-queue (UtilFeature/createQueue feat-alg 100)]
+                              ;; Detection of the features (side-effect).
+                              (.detect feat-alg img)
+                              (dotimes [i (.getNumberOfFeatures feat-alg)]
+                                (.add pos-array (.copy (.getLocation feat-alg i)))
+                                (.setTo (.grow desc-queue) 
+                                        (.getDescription feat-alg i)))
+                              [pos-array desc-queue]))
+        ;; Interested points of the first image.
+        [positions-src descriptions-src] (get-interested-pt (:mat img1))
+        ;; Interested points of the desired image.
+        [positions-dest descriptions-dest] (get-interested-pt (:mat img2))
+        ;; The association method.
+        associate (FactoryAssociation/greedy scorer 1 true)
+        ]
+    ;; Associating the features between the two images.
+    (.setSource associate descriptions-src)
+    (.setDestination associate descriptions-dest)
+    (.associate associate)
+    ;; Visualization for debug.
+    (let [panel (AssociationPanel. 20)]
+      (.setAssociation panel positions-src positions-dest (.getMatches associate))
+      (.setImages panel (h/to-buffered-image img1) (h/to-buffered-image img2))
+      (ShowImages/showWindow panel "Associated features"))
+    )
+  )
 
 (defn get-matches
-  [bimg1 bimg2]
+  []
   (let [ddp (FactoryDetectDescribe/surfStable 
               (ConfigFastHessian. 1 2 200 1 9 4 4)
               nil
               nil 
-              ImageFloat32)
+              ImageSInt32)
         scorer (FactoryAssociation/scoreEuclidean SurfFeature true)
-        associate (FactoryAssociation/greedy scorer 1 true)
-        ])
+        ]
+    (get-associations (p/to-gray timg1) (p/to-gray timg2) ddp scorer)
+    )
   )
+
+(def timg1 (h/load-file-image "/home/andre/Dropbox/Photos/banknotes_samples/20reais_old.jpg"))
+(def timg2 (h/load-file-image "/home/andre/Dropbox/Photos/banknotes_samples/natural_100.jpg"))
