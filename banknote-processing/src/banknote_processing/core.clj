7c13f27f0f0c3917eb2d4fa19a301947e3f52ab8
Andre Ambrosio Boechat <boechat107@gmail.com>
2013-12-11 19:37:49 -0200

    Return of a vec of AssociatedPair instead of a map
    * The association function returns just a vector of AssociatedPair
    instead of map containing "useless" info.

--- banknote-processing/src/banknote_processing/core.clj
+++ banknote-processing/src/banknote_processing/core.clj
@@ -8,32 +8,31 @@
      ])
   (:import 
     [boofcv.factory.feature.detdesc FactoryDetectDescribe]
-    [boofcv.abst.feature.detdesc DetectDescribePoint]
     [boofcv.factory.feature.associate FactoryAssociation]
+    [boofcv.abst.feature.detdesc DetectDescribePoint]
     [boofcv.abst.feature.associate ScoreAssociation]
     [boofcv.abst.feature.detect.interest ConfigFastHessian]
-    [boofcv.struct.feature SurfFeature TupleDesc]
+    [boofcv.struct.feature SurfFeature TupleDesc AssociatedIndex]
     [boofcv.struct.image ImageFloat32 ImageSInt32]
+    [boofcv.struct.geo AssociatedPair]
     [boofcv.alg.feature UtilFeature]
-    [java.util ArrayList]
     [boofcv.gui.feature AssociationPanel]
     [boofcv.gui.image ShowImages]
+    [java.util ArrayList]
     ))
 
 (set! *warn-on-reflection* true)
 (set! *unchecked-math* true)
 
 (defn get-association-fn
-  "Returns a function which calculates the associations between two images
-  accordingly with the feature description and scorer algorithms.
+  "Returns a function which calculates the associations between two images and
+  returns a vector of AssociatedPair, accordingly with the feature description and
+  scorer algorithms.
   If the last argument is :debug, a window is showed to visulize the associations
   between the images.
   Ex.:
   (let [assoc-fn (get-association-fn fa sc)]
-    (assoc-fn img1 img2))
-  => {:association associate 
-      :src-points positions-src
-      :dst-points positions-dest}"
+    (assoc-fn img1 img2))"
   [^DetectDescribePoint feat-alg scorer & opts]
   (let [;; Function to detect the interested points of an image and returns their
         ;; locations and descriptions.
@@ -51,27 +50,29 @@
         associate (FactoryAssociation/greedy scorer 1 true) 
         debug (some #(= :debug %) opts)]
     (fn [img-src img-dst]
-      ;; Returns the AssociateDescription object and the source/destination points 
-      ;; positions.
+      ;; Returns a vector of AssociatedPair.
       (let [gray-src (p/to-gray img-src)
             gray-dst (p/to-gray img-dst)
             ;; Interested points of the first image.
             [positions-src descriptions-src] (get-interested-pt (:mat gray-src))
             ;; Interested points of the desired image.
-            [positions-dest descriptions-dest] (get-interested-pt (:mat gray-dst))]
+            [positions-dst descriptions-dst] (get-interested-pt (:mat gray-dst))]
         ;; Associating the features between the two images.
         (.setSource associate descriptions-src)
-        (.setDestination associate descriptions-dest)
+        (.setDestination associate descriptions-dst)
         (.associate associate)
         ;; Visualization for debug.
         (when debug
           (let [panel (AssociationPanel. 20)]
-            (.setAssociation panel positions-src positions-dest (.getMatches associate))
+            (.setAssociation panel positions-src positions-dst (.getMatches associate))
             (.setImages panel (h/to-buffered-image gray-src) (h/to-buffered-image gray-dst))
             (ShowImages/showWindow panel "Associated features")))
-        {:association associate 
-         :src-points positions-src
-         :dst-points positions-dest}))))
+        ;; Creation of a vector of associated pairs.
+        (let [matches (.getMatches associate)]
+          (mapv #(let [^AssociatedIndex a-idx (.get matches %)]
+                   (AssociatedPair. (.get ^ArrayList positions-src (.src a-idx))
+                                    (.get ^ArrayList positions-dst (.dst a-idx))))
+                (range (.size matches))))))))
 
 (def timg1 (h/load-file-image "/home/andre/Dropbox/Photos/banknotes_samples/20reais_old.jpg"))
 (def timg2 (h/load-file-image "/home/andre/Dropbox/Photos/banknotes_samples/natural_100.jpg"))
