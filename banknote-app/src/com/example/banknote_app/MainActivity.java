916f1f33c6c3cb8e41abb97bfa5f288255defbc0
Andre Ambrosio Boechat <boechat107@gmail.com>
2014-01-19 19:35:13 -0200

    Finished the basic tutorial "Building your first app"
    Next step:
    http://developer.android.com/training/camera/photobasics.html

--- banknote-app/src/com/example/banknote_app/MainActivity.java
+++ banknote-app/src/com/example/banknote_app/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.banknote_app;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	public final static String EXTRA_MESSAGE = "banknotes_app.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,14 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void sendMessage(View view) {
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editText = (EditText) findViewById(R.id.edit_message);
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent);
     }
     
 }
