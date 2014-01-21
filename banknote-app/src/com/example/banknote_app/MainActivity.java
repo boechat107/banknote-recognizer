7db40fb5920cc363274d666a703142c868f013ff
Andre Ambrosio Boechat <boechat107@gmail.com>
2014-01-21 12:54:34 -0200

    Button and ImageViewer, just the thumbnail is used (testing)
    A button to take a snapshot and an Imageviewer were added and
    they are working as expected.

--- banknote-app/src/com/example/banknote_app/MainActivity.java
+++ banknote-app/src/com/example/banknote_app/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.banknote_app;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 	public final static String EXTRA_MESSAGE = "banknotes_app.MESSAGE";
+	private final static int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +28,29 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+    		Bundle extras = data.getExtras();
+    		Bitmap imgBitmap = (Bitmap) extras.get("data");
+    		ImageView imgViewer = (ImageView) findViewById(R.id.photo_viewer);
+    		imgViewer.setImageBitmap(imgBitmap);
+    	}
+    }
+    
     public void sendMessage(View view) {
     	Intent intent = new Intent(this, DisplayMessageActivity.class);
     	EditText editText = (EditText) findViewById(R.id.edit_message);
     	String message = editText.getText().toString();
     	intent.putExtra(EXTRA_MESSAGE, message);
     	startActivity(intent);
+    }
+    
+    public void takePhoto(View view) {
+    	Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    	// Checks if there is an app to handle the intent.
+    	if (intent.resolveActivity(getPackageManager()) != null)
+    		startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
     }
     
 }
